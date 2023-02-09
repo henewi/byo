fa63637f581d0f1107622309c1696f19106724d6
henewi <hpdunn2002@gmail.com>
2023-02-09 17:29:39 +0000

    working server

--- javascript.js
+++ javascript.js
@@ -1,3 +1,10 @@
 const express = require('express');
 const app = express();
 
+app.get('/', function(req, res) {
+  res.send("Hello world!")
+});
+
+var listener = app.listen(8888, function(){
+  console.log('Server is listening on port ' + listener.address().port + '...');
+});
