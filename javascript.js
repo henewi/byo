ae42d8e3f686bebebda2d518cc8a042622a4d7bf
henewi <hpdunn2002@gmail.com>
2023-02-09 17:24:34 +0000

    express

--- javascript.js
+++ javascript.js
@@ -1,6 +1,3 @@
-var http = require('http');
+const express = require('express');
+const app = express();
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('tinklywinks!');
-}).listen(8080)
