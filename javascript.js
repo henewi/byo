d46cd19e0755c1f724198727550220272f20c2f3
henewi <hpdunn2002@gmail.com>
2023-02-09 17:40:00 +0000

    added path

--- javascript.js
+++ javascript.js
@@ -1,10 +1,20 @@
+// Declaring dependancies
+
 const express = require('express');
 const app = express();
 
+// Main server and listener, http://localhost:8888/ 
+
 app.get('/', function(req, res) {
-  res.send("Hello world!")
+  res.send('Dingleberry has arrived on port ' + listener.address().port + '!')
 });
 
 var listener = app.listen(8888, function(){
   console.log('Server is listening on port ' + listener.address().port + '...');
 });
+
+// Testing with another path, http://localhost:8888/path
+
+app.get('/path'), function(req, res) {
+  res.send('You reached the path page, cheeky don!')
+}
